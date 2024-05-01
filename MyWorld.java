98e2a1b5ed5ef7b926d88bb508e01899cb3c3e5c
ElenaZ-12 <166631491+ElenaZ-12@users.noreply.github.com>
2024-05-01 12:14:24 -0400

    Created Elephant and Apple

--- MyWorld.java
+++ MyWorld.java
@@ -1,10 +1,10 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class MyWorld here.
+ * The World our hero lives in.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author (Elena Z.) 
+ * @version (May 1st 2024)
  */
 public class MyWorld extends World
 {
@@ -17,5 +17,18 @@ public class MyWorld extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1);
+        prepare();
+    }
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+        Elephant elephant = new Elephant();
+        addObject(elephant,50,200);
+        
+        Apple apple = new Apple();
+        addObject(apple, 300, 0);
     }
 }
