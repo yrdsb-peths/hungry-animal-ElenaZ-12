dfbce249003c5b592925f8287677a951ea5700b2
ElenaZ-12 <166631491+ElenaZ-12@users.noreply.github.com>
2024-05-01 12:20:39 -0400

    Elephant movement and collision

--- Apple.java
+++ Apple.java
@@ -1,10 +1,10 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class Apple here.
+ * Food for our elephant.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author (Elena Z.) 
+ * @version (May 1st, 2024)
  */
 public class Apple extends Actor
 {
@@ -14,6 +14,6 @@ public class Apple extends Actor
      */
     public void act()
     {
-        // Add your action code here.
+        setLocation(getX(),getY()+1);
     }
 }
--- Elephant.java
+++ Elephant.java
@@ -14,6 +14,17 @@ public class Elephant extends Actor
      */
     public void act()
     {
-        move(1);
+        if(Greenfoot.isKeyDown("left"))
+        {
+            move(-3);
+        }
+        else if (Greenfoot.isKeyDown("right"))
+        {
+            move(3);
+        }
+        
+        //Remove apple if elephant eats it
+        removeTouching(Apple.class);
+        
     }
 }
