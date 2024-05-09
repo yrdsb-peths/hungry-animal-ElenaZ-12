2ccf75a4b82142170c8760164acbe8901ca331e3
ElenaZ-12 <166631491+ElenaZ-12@users.noreply.github.com>
2024-05-09 00:07:44 -0400

    animate elephant

--- Elephant.java
+++ Elephant.java
@@ -9,6 +9,29 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class Elephant extends Actor
 {
     GreenfootSound elephantSound = new GreenfootSound("elephantcub.mp3");
+    GreenfootImage[] idle = new GreenfootImage[8];
+    /**
+     * Constructor - The code that gets run one time when object is created
+     */
+    public Elephant()
+    {
+        for (int i = 0; i<idle.length; i++)
+        {
+            idle[i] = new GreenfootImage ("images/elephant_idle/idle" + i + ".png");
+        }
+        setImage(idle[0]);
+    }
+    
+    /**
+     * Animate the elephant
+     */
+    int imageIndex = 0;
+    public void animateElephant()
+    {
+        setImage(idle[imageIndex]);
+        imageIndex = (imageIndex + 1) % idle.length;
+    }
+    
     public void act()
     {
         if(Greenfoot.isKeyDown("left"))
@@ -22,6 +45,9 @@ public class Elephant extends Actor
         
         //Remove apple if elephant eats it
         eat();
+        
+        //Animate the elephant
+        animateElephant();
     }
     
     /**
