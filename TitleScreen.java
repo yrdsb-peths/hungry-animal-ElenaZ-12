871f5b19296cec3aa3aabfc77b4df0ea97ae2c53
ElenaZ-12 <166631491+ElenaZ-12@users.noreply.github.com>
2024-05-13 12:05:28 -0400

    Title screen instructions + elephant

--- TitleScreen.java
+++ TitleScreen.java
@@ -17,10 +17,11 @@ public class TitleScreen extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
-        
+
         addObject(titleLabel, getWidth()/2, getHeight()/2);
+        prepare();
     }
-    
+
     /**
      * The main world act loop
      */
@@ -32,5 +33,19 @@ public class TitleScreen extends World
             MyWorld gameWorld = new MyWorld();
             Greenfoot.setWorld(gameWorld);
         }
+    }
+
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+        Elephant elephant = new Elephant();
+        addObject(elephant, getWidth()/2, getHeight()/2-80);
+        Label label = new Label("Use <-- and --> to move", 30);
+        addObject(label,310,250);
+        Label label2 = new Label("Press space to start", 30);
+        addObject(label2,306,284);
     }
 }
